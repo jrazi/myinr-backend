5f58ae5749072932ea8f0f6bf2f3c7d87f9febef
jrazi <j.razi@outlook.com>
2021-04-10 16:36:30 +0000

    stash

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
     deploy : {
         production : {
             user : 'ubuntu',
-            host : '37.152.185.234',
+            host : 'localhost',
             ref  : 'origin/master',
             repo : 'https://jrazi@github.com/jrazi/myinr-backend.git',
-            path : '/home/ubuntu/misd/myinr-backend',
+            path : '',
             'pre-deploy-local': '',
             'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
             'pre-setup': '',
